c0f7501e2af7e124ac1f1c2c2e1e3e6043ec0428
DionKoenes <koenesdion@gmail.com>
2022-11-10 14:57:09 +0100

    Toggle Button
    Toggle button gemaakt. De button werke wel met activeren en deactiveren van een gameobject maar alleen op pressed en release, dus niet op toggle. Het duurde me een tijdje om uit te vogelen hoe ik dat in mijn code goed kon krijgen maar heb uiteindelijk hier een geluidje en toggle aan vast kunnen maken

--- Assets/PhysicsButton.cs
+++ Assets/PhysicsButton.cs
@@ -7,18 +7,28 @@ public class PhysicsButton : MonoBehaviour
 {
     [SerializeField] private float threshold = 0.1f;
     [SerializeField] private float deadZone = 0.025f;
+    
+    [SerializeField] private AudioClip buttonSound;
+    private AudioSource source;
 
     private bool isPressed;
     private Vector3 startPos;
     private ConfigurableJoint joint;
+
+    private bool hasPlayed = false;
+
+    [SerializeField] private GameObject spotLight;
+    private bool lampIsOff = true;
 
     public UnityEvent onPressed, onReleased;
 
     // Start is called before the first frame update
     void Start()
     {
+        source = gameObject.AddComponent<AudioSource>();
         startPos = transform.localPosition;
         joint = GetComponent<ConfigurableJoint>();
+        spotLight = GameObject.Find("Spotlight").GetComponent<GameObject>();
     }
 
     // Update is called once per frame
@@ -26,6 +36,8 @@ public class PhysicsButton : MonoBehaviour
     {
         if (!isPressed && GetValue() + threshold >= 1)
             Pressed();
+            ToggleLight();
+
         if (isPressed && GetValue() - threshold <= 0)
             Released();
     }
@@ -52,5 +64,24 @@ public class PhysicsButton : MonoBehaviour
         isPressed = false;
         onReleased.Invoke();
         Debug.Log("Released");
+        hasPlayed = false;
+    }
+
+    private void ToggleLight()
+    {
+        if (!hasPlayed && isPressed && lampIsOff)
+        {
+            source.PlayOneShot(buttonSound);
+            hasPlayed = true;
+            spotLight.SetActive(true);
+            lampIsOff = false;
+        }
+        else if (!hasPlayed && isPressed && !lampIsOff)
+        {
+            source.PlayOneShot(buttonSound);
+            hasPlayed = true;
+            spotLight.SetActive(false);
+            lampIsOff = true;
+        }
     }
 }
