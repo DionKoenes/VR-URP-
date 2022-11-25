160a9e9c32d413054e008f842e9db47e2fda95fa
DionKoenes <koenesdion@gmail.com>
2022-11-25 14:24:03 +0100

    Commit eind week
    Deze week ben ik enorm hard bezig geweest met het proberen te maken van een serie schakeling voor elektriciteit. Aan het eind van de week heb ik nu een grof beeld gekregen van wat ik volgende week moet gaan doen om dit realiteit te maken.

---
+++ Assets/SerieCircuit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerieCircuit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* volt op batterij, wanneer anchorbrown in contact komt met batterijplus zet kabel volt op 5 bijv. 
+    Wanneer deze andere achor in contact komt met knop negatief. 
+    Dan pas kan de knop de andere lamp aan doen wanneer deze ook aan de lamp zit met een zwarte kabel*/
+}
---
+++ Assets/SwitchControll.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchControll : MonoBehaviour
+{
+    //Sound
+    private AudioSource source;
+    [SerializeField] private AudioClip switchSound;
+
+    private bool on = false;
+    [SerializeField] private bool switchHit = false;
+
+    private float switchRotation = 180;
+    [SerializeField] private GameObject switchBase;
+
+    [SerializeField] private GameObject spotLight;
+
+    void Start()
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        //turn off spotlight
+        spotLight.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (switchHit == true)
+        {
+            //PlaySound
+            source.PlayOneShot(switchSound);
+            switchHit = false;
+            //if on is true, make on false. And if on is false, make on true.
+            on = !on;
+
+            if (on == true)
+            {
+                spotLight.SetActive(true);
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x + switchRotation, transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+            else
+            {
+                spotLight.SetActive(false);
+                transform.rotation = Quaternion.Euler(transform.eulerAngles.x - switchRotation, transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Left Hand") || other.CompareTag("Right Hand"))
+        {
+            switchHit = true;
+        }
+    }
+}
+
